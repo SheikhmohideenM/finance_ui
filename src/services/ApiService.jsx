8adbee1f43316f72703ecdba4eeef83a314727bc
sheik <sheikmagfinserv@gmail.com>
2026-01-31 02:19:34 +0530

    render com has added in vite.config.js

--- src/services/ApiService.jsx
+++ src/services/ApiService.jsx
@@ -1,8 +1,10 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'http://localhost:3000/api/v1'
+// const API_BASE_URL = 'http://localhost:3000/api/v1' for local
+const API_BASE_URL = import.meta.env.VITE_API_URL
 
 const apiClient = axios.create({
+  // baseURL: API_BASE_URL, for local
   baseURL: API_BASE_URL,
   withCredentials: true,
   headers: {
