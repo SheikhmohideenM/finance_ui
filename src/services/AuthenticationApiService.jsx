e9ea5d67f6c6500f8173fa12a7fd668f255b2f71
sheik <sheikmagfinserv@gmail.com>
2026-01-31 10:55:21 +0530

    login and signup auth file changed

--- src/services/AuthenticationApiService.jsx
+++ src/services/AuthenticationApiService.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
-const API_BASE_URL = 'http://localhost:3000' //for local
-// const API_BASE_URL = import.meta.env.VITE_API_URL_LOGIN_SIGNUP
+// const API_BASE_URL = 'http://localhost:3000' //for local
+const API_BASE_URL = import.meta.env.VITE_API_URL_LOGIN_SIGNUP
 
 const apiClient = axios.create({
   baseURL: API_BASE_URL,
